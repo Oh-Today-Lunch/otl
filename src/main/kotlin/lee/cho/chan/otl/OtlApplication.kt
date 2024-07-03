81a2e8fdf0bc023e4be76121c3d146fe9a2bae9e
youngmi <youngmi.cho@nflux.co.kr>
2024-07-03 22:57:54 +0900

    Initial commit

---
+++ src/main/kotlin/lee/cho/chan/otl/OtlApplication.kt
@@ -0,0 +1,11 @@
+package lee.cho.chan.otl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OtlApplication
+
+fun main(args: Array<String>) {
+    runApplication<OtlApplication>(*args)
+}
---
+++ src/test/kotlin/lee/cho/chan/otl/OtlApplicationTests.kt
@@ -0,0 +1,13 @@
+package lee.cho.chan.otl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OtlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
