7dd14b87c8497b5ddbc2a4055f9e10223e6e7d08
youngmi <youngmi.cho@nflux.co.kr>
2024-08-26 22:38:15 +0900

    feat: temp

---
+++ src/main/kotlin/lee/cho/chan/otl/enum/Category.kt
@@ -0,0 +1,8 @@
+package lee.cho.chan.otl.enum
+
+enum class Category(category:String) {
+    CHINESE("중식당"),
+    KOREAN("한식당"),
+    WESTERN("양식당"),
+    JAPANESE("일식당")
+}
---
+++ src/main/kotlin/lee/cho/chan/otl/enum/Station.kt
@@ -0,0 +1,5 @@
+package lee.cho.chan.otl.enum
+
+enum class Station(station: String) {
+    SUNAE("수내역")
+}
